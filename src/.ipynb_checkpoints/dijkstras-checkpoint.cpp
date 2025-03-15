801eb60717ad470cbfe50e0e919892e613bf64d8
HarryS561 <2608771343@qq.com>
2025-03-15 03:56:28 +0000

    1

--- src/.ipynb_checkpoints/dijkstras-checkpoint.cpp
+++ src/.ipynb_checkpoints/dijkstras-checkpoint.cpp
@@ -47,8 +47,7 @@ void print_path(const vector<int>& path, int total) {
     }
     
     for (size_t i = 0; i < path.size(); ++i) {
-        if (i > 0) cout << " -> ";
-        cout << path[i];
+        cout << path[i] << " ";
     }
-    cout << " (Cost: " << total << ")" << endl;
+    cout << endl << "Total cost is " << total << endl;
 }
