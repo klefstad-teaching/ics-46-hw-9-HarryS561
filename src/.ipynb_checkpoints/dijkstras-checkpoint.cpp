702f8906f52002b1476b8f402834f364fb41aab6
HarryS561 <2608771343@qq.com>
2025-03-15 04:12:25 +0000

    1

--- src/.ipynb_checkpoints/dijkstras-checkpoint.cpp
+++ src/.ipynb_checkpoints/dijkstras-checkpoint.cpp
@@ -41,7 +41,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& path, int total) {
-    if (!total) {
+    if (path.empty()) {
         cout << "No path found." << endl;
         return;
     }
